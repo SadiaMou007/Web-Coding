989968177f15dd62a095746527dce1dfd83a77d2
Your Name <you@example.com>
2022-02-22 00:09:55 -0800

    obj destructuring

---
+++ M32-ES6-Advance/objectDestructuring.js
@@ -0,0 +1,19 @@
+// Simple example //
+const obj = {
+  name: "Sadia",
+  address: "Dhaka",
+  phone: "01982075321",
+  status: "Tired Now",
+};
+const { name, address, phone, status } = obj;
+console.log(phone, name, status);
+
+// Complex example //
+const company = {
+  cName: "Bjit",
+  ceo: { name: "Mr Z", phone: "01717XXX" },
+  employee: { female: "22", male: 22 },
+  framework: { frontend: "react", backend: "vary" },
+};
+const { cName, ceo, framework } = company;
+console.log(cName, ceo.phone, framework.backend);
