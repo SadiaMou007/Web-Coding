4a9c50e3aeb991c1ac025e2b88333680bd108b81
Your Name <you@example.com>
2022-03-10 00:31:22 -0800

    debug muisic corner

--- M42-debug-4-practice-project/miujik-src/script.js
+++ M42-debug-4-practice-project/miujik-src/script.js
@@ -1,9 +1,10 @@
 const elementById = (id) => {
-  document.getElementById(id);
+  return document.getElementById(id);
 };
 
 const handleSearch = () => {
   const keyword = elementById("keyword");
+
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
   fetch(url)
     .then((res) => res.json())
@@ -11,14 +12,23 @@ const handleSearch = () => {
 };
 
 const showArtists = (data) => {
-  const artistContainer = elementById("artist");
+  const artistContainer = elementById("artists");
+  const albumContainer = elementById("albums");
+
+  artistContainer.innerHTML = "";
+  albumContainer.innerHTML = "";
+
   data?.artists?.forEach((artist) => {
     const div = document.createElement("div");
     div.classList.add("artist-card");
     div.innerHTML = `<div class="image-container">
     <div class="image-container-inner">
       <img
-        src="${artist.strArtistThumb}"
+        src="${
+          artist.strArtistThumb
+            ? artist.strArtistThumb
+            : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png"
+        }"
         alt=""
       />
     </div>
@@ -30,16 +40,18 @@ const showArtists = (data) => {
   </div>
   <button class="album-button">
     <i class="fa-solid fa-compact-disc"></i>
-    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+    <p onclick="fetchAlbums('${
+      artist.idArtist
+    }')" class="button-title">Albums</p>
   </button>`;
     artistContainer.appendChild(div);
   });
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
   fetch(url)
-    .then((res) => res.JSON())
+    .then((res) => res.json())
     .then((data) => showAlbum(data));
   const artistContainer = elementById("artists");
   artistContainer.innerHTML = "";
@@ -47,18 +59,18 @@ const fetchAlbums = (id) => {
 
 const showAlbum = (data) => {
   const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  data.album.forEach((item) => {
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${album.strAlbumThumb}"
+            src="${item.strAlbumThumb}"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${album.strAlbum}</h3>
+          <h3>${item.strAlbum}</h3>
         </div>
       `;
 
