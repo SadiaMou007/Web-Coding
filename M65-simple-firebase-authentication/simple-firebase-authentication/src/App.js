51a6f981d24731450abe934825773f0efbd81b33
Your Name <you@example.com>
2022-04-06 21:49:08 -0700

    firebase auth

---
+++ M65-simple-firebase-authentication/simple-firebase-authentication/src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import app from "./firebase.init";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+const auth = getAuth(app);
+function App() {
+  const [user, setUser] = useState({});
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      });
+  };
+
+  return (
+    <div className="App">
+      <h2>please ALLAH help us</h2>
+
+      {user.email ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google sign in</button>
+      )}
+
+      <h2>UserName: {user.displayName}</h2>
+      <p>Email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
+    </div>
+  );
+}
+
+export default App;
