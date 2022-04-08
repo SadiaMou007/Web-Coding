4a30bb72362f57734e7facec656b6b018b277dbf
Your Name <you@example.com>
2022-04-08 00:04:53 -0700

    vali

--- M57-email-password-authentication/email-pass-auth/src/App.js
+++ M57-email-password-authentication/email-pass-auth/src/App.js
@@ -10,6 +10,9 @@ import app from "./firebase.init";
 const auth = getAuth(app);
 
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
+
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
 
@@ -24,29 +27,59 @@ function App() {
   };
 
   const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      return;
+    }
+    if (
+      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/.test(
+        pass
+      )
+    ) {
+      e.stopPropagation();
+      setError(
+        "Password requires at least 1 uppercase, 1 lowercase, 1 number and 1 special character"
+      );
+      return;
+    }
+
+    setValidated(true);
+    setError("");
+
     createUserWithEmailAndPassword(auth, email, pass)
       .then((result) => {
         const user = result.user;
+        setEmail("");
+        setPass("");
         console.log(user);
       })
       .catch((error) => {
+        setError(error.message);
         console.log(error);
       });
-    e.preventDefault();
   };
 
   return (
     <div className="App">
       <h2 className="m-3 text-center">Email pass auth(sign up)</h2>
+
       <div className="w-50 mx-auto p-4 bg-light rounded">
-        <Form onSubmit={handleFormSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
               onBlur={handleEmailBlur}
               type="email"
               placeholder="Enter email"
+              required
             />
+            <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -54,11 +87,18 @@ function App() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
+
             <Form.Control
               onBlur={handlePasswordBlur}
               type="password"
               placeholder="Password"
+              required
             />
+            <p className="text-danger">{error}</p>
+
+            <Form.Control.Feedback type="invalid">
+              Please choose a password
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Button variant="primary" type="submit">
