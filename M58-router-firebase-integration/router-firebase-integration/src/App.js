8ba0f0e875075353893cc71c76eefb6dce7ebdb9
Your Name <you@example.com>
2022-04-10 12:50:59 -0700

    add private route

--- M58-router-firebase-integration/router-firebase-integration/src/App.js
+++ M58-router-firebase-integration/router-firebase-integration/src/App.js
@@ -6,6 +6,8 @@ import Login from "./components/Login/Login";
 import Products from "./components/Products/Products";
 import Reviews from "./components/Reviews/Reviews";
 import Register from "./components/Register/Register";
+import Orders from "./components/Orders/Orders";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 function App() {
   return (
@@ -16,6 +18,14 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/products" element={<Products></Products>}></Route>
         <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders></Orders>
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
--- M58-router-firebase-integration/router-firebase-integration/src/components/Header/Header.js
+++ M58-router-firebase-integration/router-firebase-integration/src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   console.log(user);
   return (
     <div>
-      <div className="border-2 p-4 text-xl font-mono flex justify-between">
+      <div className="border-2 p-4 font-bold font-mono flex justify-between">
         <div className="hidden md:block">NavBrand</div>
         <div className="flex">
           <div className="mx-3">
@@ -43,6 +43,16 @@ const Header = () => {
               to={"/reviews"}
             >
               Reviews
+            </NavLink>
+          </div>
+          <div className="mx-3">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "text-cyan-500" : "text-cyan-700"
+              }
+              to={"/orders"}
+            >
+              Orders
             </NavLink>
           </div>
           <div className="mx-3">
